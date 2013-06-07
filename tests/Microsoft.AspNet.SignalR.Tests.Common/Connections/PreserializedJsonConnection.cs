29bfec1988208bc2fe607dfad7721a8039d34fb2
Stephen Halter <halter73@gmail.com>
2013-06-07 14:39:32 -0700

    Test sending preserialized json from a PersistentConnection
    #1546

---
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Connections/PreserializedJsonConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.SignalR.Tests.Common.Connections
+{
+    public class PreserializedJsonConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            var jsonBytes = new ArraySegment<byte>(Encoding.UTF8.GetBytes("{\"preserialized\": true}"));
+
+            Connection.Send(connectionId, jsonBytes);
+            return base.OnConnected(request, connectionId);
+        }
+    }
+}
